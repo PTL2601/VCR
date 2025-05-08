0501be2e7c27736e59f79f313a7ba08fde45100d
PTL2601 <161822779+PTL2601@users.noreply.github.com>
2025-05-08 17:36:20 +0300

    requirements+

--- STT/main.py
+++
@@ -1,6 +0,0 @@
-from fuzzywuzzy import fuzz
-import STT
-
-stt = STT("57816563491305416.mp3", model_path=".")
-# recognized_text1 = stt.decode_audio()
-
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import id
-import decode
+from STT import id
+from STT import STT
 
 audio_file = "57816563491305416.wav" #!
 caller_number_catch = "cdr_description.html" #!
 
 extractor = id.CallerNumberExtractor(caller_number_catch)
 
-stt = decode.STT(audio_file, model_path=".") #!model_path!
+stt = STT.STT(audio_file, model_path="C:/Users/Denis Beryozkin/Desktop/local test") #!model_path!
 extractor.load_and_clean_html()
 
 caller_number = extractor.extract_caller_number()
